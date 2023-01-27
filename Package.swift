75a160a6e8e5ee443b9ec7b30d2fa690928c9b10
Konstantin Dorogan <sayd3x@users.noreply.github.com>
2023-01-27 21:54:24 +0300

    Fixed the Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OpenCV",
-            targets: ["OpenCV"]),
+            targets: ["opencv2"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "OpenCV",
+            name: "opencv2",
             url: "https://github.com/swipetospin/openCV-SpinCar/releases/download/4.6.0/opencv2.xcframework.zip",
             checksum: "6b4c436c51083796aa7e49935f4225518decac0bcc30a93172791311ee6d08e1"
         )
